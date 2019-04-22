54720b80d019c04e8c66a15ae32d25dbdf2e3d2f
songdingxx <songdingxx@gmail.com>
2019-04-22 20:56:17 +0800

    save

---
+++ precache-manifest.083b823139a3e80d83946081837473ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4bd721b5c80e746fa12a",
+    "url": "/static/js/main.1888ffcf.chunk.js"
+  },
+  {
+    "revision": "5e2dfcba770b7991f282",
+    "url": "/static/js/2.5d786819.chunk.js"
+  },
+  {
+    "revision": "4bd721b5c80e746fa12a",
+    "url": "/static/css/main.0875ccdd.chunk.css"
+  },
+  {
+    "revision": "44923e0986cad3710fe269c93b5ad72b",
+    "url": "/index.html"
+  }
+];
